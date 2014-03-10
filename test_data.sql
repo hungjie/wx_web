46e8512b0e1e6ff18e9ebd97b04dc2c2338efeb3
liuhongzhao <liuhongzhao@zingmobile.com>
2014-03-10 15:19:58 +0800

    update sql for insert meal

--- test_data.sql
+++ test_data.sql
@@ -1,6 +1,6 @@
 USE `wx_sms`;
 SET NAMES utf8;
---insert into meal_list(name, img_path, count, price) values('红烧牛肉饭', '/image/head.jpg', 10, 10.00),('红烧排骨套餐','/image/head.jpg', 10, 12.00),('西红柿蛋饭', '/image/head.jpg',10, 9.00);
+
 insert into meal_list(name, img_path, count, price) values('红烧猪脚', '/image/hszj.jpg', 10, 14.00);
 insert into meal_list(name, img_path, count, price) values('红烧带鱼', '/image/hsdy.jpg', 10, 14.00);
 insert into meal_list(name, img_path, count, price) values('咸水鸭', '/image/xsy.jpg', 10, 14.00);
